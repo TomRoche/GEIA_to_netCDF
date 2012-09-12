f982de0660b10f380183e34a0f1557a4cb1c5bb7
Tom Roche <Tom_Roche@pobox.com>
2012-09-12 16:02:14 -0400

    netCDF.stats.to.stdout.r is "module-pythonic": can be run as script or function
    Thanks to Trevor Davis (see
    https://stat.ethz.ch/pipermail/r-help/2012-September/323559.html
    ) one can now
    me@it:/path/to/GEIA_to_netCDF $ Rscript ./netCDF.stats.to.stdout.r netcdf.fp=./GEIA_N2O_oceanic.nc var.name=emi_n2o
    > For ./GEIA_N2O_oceanic.nc var=emi_n2o
    > cells=64800
    > obs=36143
    > min=5.96e-08
    > q1=30.4
    > med=67.7
    > mean=99.5
    > q3=140
    > max=1.17e+03
    me@it:/path/to/GEIA_to_netCDF $ R
    > R version 2.14.1 (2011-12-22)
    ...
    > > source('./netCDF.stats.to.stdout.r')
    > > netCDF.stats.to.stdout(netcdf.fp=./GEIA_N2O_oceanic.nc, var.name=emi_n2o)
    > Error in nc_open(filename = netcdf.fp, write = FALSE, readunlim = TRUE) :
    >   object '.' not found
    
    oops! in function, gotta quote the args:
    
    > > netCDF.stats.to.stdout(netcdf.fp='./GEIA_N2O_oceanic.nc', var.name='emi_n2o')
    > For ./GEIA_N2O_oceanic.nc var=emi_n2o
    > cells=64800
    > obs=36143
    > min=5.96e-08
    > q1=30.4
    > med=67.7
    > mean=99.5
    > q3=140
    > max=1.17e+03

--- netCDF.stats.to.stdout.r
+++ netCDF.stats.to.stdout.r
@@ -1,7 +1,13 @@
 # R code to write simple stats (min, mean, median, max) for an input file.
 # Run this like
-# > $ Rscript ./netCDF.stats.to.stdout.r netcdf.fp=./GEIA_N2O_oceanic.nc var.name=emi_n2o
-# There's gotta be an easier way.
+# $ Rscript ./netCDF.stats.to.stdout.r netcdf.fp=./GEIA_N2O_oceanic.nc var.name=emi_n2o
+# or
+# > source('./netCDF.stats.to.stdout.r')
+# > netCDF.stats.to.stdout(...)
+
+# constants-----------------------------------------------------------
+
+this.fn <- 'netCDF.stats.to.stdout.r'      # TODO: get like $0
 
 # functions-----------------------------------------------------------
 
@@ -9,136 +15,171 @@
 q1 <- function(vec) { quantile(vec, 0.25) } # first quartile
 q3 <- function(vec) { quantile(vec, 0.75) } # third quartile
 
-# constants-----------------------------------------------------------
+# the main event
+netCDF.stats.to.stdout <- function(
+  netcdf.fp, # /path/to/netcdf/file, can be relative or FQ
+  var.name   # name_of_data_variable
+) {
 
-# TODO: fix `strsplit` regexp below to make this unnecessary
-library(stringr)
-
-this.fn <- 'netCDF.stats.to.stdout.r'      # TODO: get like $0
-
-# input metadata: pass via `Rscript args`
-netcdf.fp <- '/unknown/path/to/netcdf/file' # can be relative or FQ
-var.name <- 'unknown_name_of_data_variable'
-
-# pass named arguments: var above separated by '='
-# TODO: also support positional
-args <- commandArgs(TRUE)
-# args is now a list of character vectors
-# First check to see if any arguments were passed, then evaluate each argument:
-# assign val (RHS) to key (LHS) for arguments of the (required) form 'key=val'
-if (length(args)==0) {
-  cat("No arguments supplied\n")
-  q(status=1) # exit
-} else {
-# simple positional args work
-# TODO: allow this usage
-#  netcdf.fp <- args[1]
-#  var.name <- args[2]
-  # TODO: test arg length: 2 is required!
-  for (i in 1:length(args)) {
-#    eval(parse(text=args[[i]]))
-# `eval(parse())` is unsafe and requires awkward quoting:
-# e.g., of the following (bash) commandlines
-
-# - Rscript ./netCDF.stats.to.stdout.r netcdf.fp="GEIA_N2O_oceanic.nc" var.name="emi_n2o"
-#   fails
-
-# + Rscript ./netCDF.stats.to.stdout.r 'netcdf.fp="GEIA_N2O_oceanic.nc"' 'var.name="emi_n2o"'
-#   succeeds
-    args.keyval.list <-
-      strsplit(as.character(parse(text=args[[i]])),
-        split='[[:blank:]]*<-|=[[:blank:]]*', fixed=FALSE)
-#                            split='[ \t]*<-|=[ \t]*', fixed=FALSE)
-    args.keyval.vec <- unlist(args.keyval.list, recursive=FALSE, use.names=FALSE)
-    # TODO: test vector elements!
-    # Neither wants to remove all whitespace from around arguments :-( so
-    args.key <- str_trim(args.keyval.vec[1], side="both")
-    args.val <- str_trim(args.keyval.vec[2], side="both")
-    # A real case statement would be nice to have
-    if        (args.key == 'netcdf.fp') {
-      netcdf.fp <- args.val
-    } else if (args.key == 'var.name') {
-      var.name <- args.val
-    } else {
-      stop(sprintf("unknown argument='%s'", args.key))
-      # TODO: show usage
-      q(status=1) # exit with error
-    }
-  } # end for loop over arguments
-} # end if testing number of arguments
+  # TODO: test arguments!
 
 # start debug
-# cat(sprintf('%s: netcdf.fp="%s", var.name="%s"\n', this.fn, netcdf.fp, var.name))
+#  cat(sprintf('%s: netcdf.fp==%s, var.name==%s\n', this.fn, netcdf.fp, var.name))
 #   end debug
 
-# double-sprintf-ing to set precision by constant: cool or brittle?
-stats.precision <- 3 # sigdigs to use for min, median, max of obs
-stat.str <- sprintf('%%.%ig', stats.precision)
-# use these in function=subtitle.stats as sprintf inputs
-max.str <- sprintf('max=%s', stat.str)
-mea.str <- sprintf('mean=%s', stat.str)
-med.str <- sprintf('med=%s', stat.str)  # median
-min.str <- sprintf('min=%s', stat.str)
-q1.str <- sprintf('q1=%s', stat.str)    # first quartile
-q3.str <- sprintf('q3=%s', stat.str)    # third quartile
+  # <simple input check>
+#   system(sprintf('ls -alth %s', netcdf.fp))
+#   system(sprintf('ncdump -h %s', netcdf.fp))
+  # </simple input check>
+
+  # double-sprintf-ing to set precision by constant: cool or brittle?
+  stats.precision <- 3 # sigdigs to use for min, median, max of obs
+  stat.str <- sprintf('%%.%ig', stats.precision)
+  # use these in function=subtitle.stats as sprintf inputs
+  max.str <- sprintf('max=%s', stat.str)
+  mea.str <- sprintf('mean=%s', stat.str)
+  med.str <- sprintf('med=%s', stat.str)  # median
+  min.str <- sprintf('min=%s', stat.str)
+  q1.str <- sprintf('q1=%s', stat.str)    # first quartile
+  q3.str <- sprintf('q3=%s', stat.str)    # third quartile
+
+  # needed to parse netCDF
+  library(ncdf4)
+
+  # open netCDF file, uncautiously
+  # NOTE: you must assign when you nc_open!
+  netcdf.file <- nc_open(
+    filename=netcdf.fp,
+    write=FALSE,    # will only read below
+    readunlim=TRUE) # it's a small file
+
+  # uncautiously get the data out of the datavar
+  var.data <- ncvar_get(
+    nc=netcdf.file,
+    varid=var.name)
+
+  # # <simple output check/>
+  # dim(var.data) # [1] 360 180
+
+  if (is.numeric(var.data) && sum(!is.na(var.data))) {
+    unsparse.data <- var.data[!is.na(var.data)]
+    obs <- length(unsparse.data)
+    if (obs > 0) {
+      cells.str <- sprintf('cells=%i', length(var.data))
+      obs.str <- sprintf('obs=%i', obs)
+      min.str <- sprintf(min.str, min(unsparse.data))
+      q1.str <- sprintf(q1.str, q1(unsparse.data))
+      mea.str <- sprintf(mea.str, mean(unsparse.data))
+      med.str <- sprintf(med.str, median(unsparse.data))
+      q3.str <- sprintf(q3.str, q3(unsparse.data))
+      max.str <- sprintf(max.str, max(unsparse.data))
+
+      cat(sprintf('For %s var=%s\n', netcdf.fp, var.name))
+      cat(sprintf('\t%s\n', cells.str))
+      cat(sprintf('\t%s\n', obs.str))
+      # 6-number summary
+      cat(sprintf('\t%s\n', min.str))
+      cat(sprintf('\t%s\n', q1.str))
+      cat(sprintf('\t%s\n', med.str))
+      cat(sprintf('\t%s\n', mea.str))
+      cat(sprintf('\t%s\n', q3.str))
+      cat(sprintf('\t%s\n', max.str))
+
+    } else {
+      cat(sprintf('%s: %s var=%s has no non-NA data',
+        this.fn, var.name, netcdf.fp))
+    }
+  } else {
+    cat(sprintf('%s: %s var=%s has no numeric non-NA data',
+      this.fn, var.name, netcdf.fp))
+  }
+
+  # teardown
+  nc_close(netcdf.file)
+
+} # end function netCDF.stats.to.stdout
 
 # code----------------------------------------------------------------
 
-# needed to parse netCDF
-library(ncdf4)
+# if this is called as a script, provide a main(): see
+# https://stat.ethz.ch/pipermail/r-help/2012-September/323551.html
+# https://stat.ethz.ch/pipermail/r-help/2012-September/323559.html
+if (!interactive()) {
 
-# # <simple input check>
-# system(sprintf('ls -alth %s', netcdf.fp))
-# system(sprintf('ncdump -h %s', netcdf.fp))
-# # </simple input check>
+# start debug
+#  cat(sprintf('%s: interactive()==TRUE\n', this.fn))
+#   end debug
+  
+  # TODO: fix `strsplit` regexp below to make this unnecessary
+  library(stringr)
 
-# open netCDF file, uncautiously
-# NOTE: you must assign when you nc_open!
-netcdf.file <- nc_open(
-  filename=netcdf.fp,
-  write=FALSE,    # will only read below
-  readunlim=TRUE) # it's a small file
+  # pass named arguments: var above separated by '='
 
-# uncautiously get the data out of the datavar
-var.data <- ncvar_get(
-  nc=netcdf.file,
-  varid=var.name)
-
-# # <simple output check/>
-# dim(var.data) # [1] 360 180
-
-if (is.numeric(var.data) && sum(!is.na(var.data))) {
-  unsparse.data <- var.data[!is.na(var.data)]
-  obs <- length(unsparse.data)
-  if (obs > 0) {
-    cells.str <- sprintf('cells=%i', length(var.data))
-    obs.str <- sprintf('obs=%i', obs)
-    min.str <- sprintf(min.str, min(unsparse.data))
-    q1.str <- sprintf(q1.str, q1(unsparse.data))
-    mea.str <- sprintf(mea.str, mean(unsparse.data))
-    med.str <- sprintf(med.str, median(unsparse.data))
-    q3.str <- sprintf(q3.str, q3(unsparse.data))
-    max.str <- sprintf(max.str, max(unsparse.data))
-
-    cat(sprintf('For %s var=%s\n', netcdf.fp, var.name))
-    cat(sprintf('\t%s\n', cells.str))
-    cat(sprintf('\t%s\n', obs.str))
-    # 6-number summary
-    cat(sprintf('\t%s\n', min.str))
-    cat(sprintf('\t%s\n', q1.str))
-    cat(sprintf('\t%s\n', med.str))
-    cat(sprintf('\t%s\n', mea.str))
-    cat(sprintf('\t%s\n', q3.str))
-    cat(sprintf('\t%s\n', max.str))
-
+  args <- commandArgs(TRUE)
+  # args is now a list of character vectors
+  # First check to see if any arguments were passed, then evaluate each argument:
+  # assign val (RHS) to key (LHS) for arguments of the (required) form 'key=val'
+  if (length(args)==0) {
+    cat(sprintf('%s: no arguments supplied, exiting\n', this.fn))
+    q(status=1) # exit
   } else {
-    cat(sprintf('%s var=%s has no non-NA data',
-      var.name, netcdf.fp))
-  }
-} else {
-  cat(sprintf('%s var=%s has no numeric non-NA data',
-    var.name, netcdf.fp))
-}
+  # simple positional args work
+  # TODO: also support positional usage
+  #  netcdf.fp <- args[1]
+  #  var.name <- args[2]
+    # TODO: test arg length: 2 is required!
 
-# teardown
-nc_close(netcdf.file)
+# start debug
+#    cat(sprintf('%s: got length(args)==%i\n', this.fn, length(args)))
+#   end debug
+
+    for (i in 1:length(args)) {
+#       eval(parse(text=args[[i]]))
+      # `eval(parse())` is unsafe and requires awkward quoting:
+      # e.g., of the following (bash) commandlines
+
+      # - Rscript ./netCDF.stats.to.stdout.r netcdf.fp="GEIA_N2O_oceanic.nc" var.name="emi_n2o"
+      #   fails
+
+      # + Rscript ./netCDF.stats.to.stdout.r 'netcdf.fp="GEIA_N2O_oceanic.nc"' 'var.name="emi_n2o"'
+      #   succeeds
+
+      # so instead
+      # TODO: use package `optparse` or `getopt`
+      args.keyval.list <-
+        strsplit(as.character(parse(text=args[[i]])),
+          split='[[:blank:]]*<-|=[[:blank:]]*', fixed=FALSE)
+  #                            split='[ \t]*<-|=[ \t]*', fixed=FALSE)
+      args.keyval.vec <- unlist(args.keyval.list, recursive=FALSE, use.names=FALSE)
+      # TODO: test vector elements!
+      # Neither wants to remove all whitespace from around arguments :-( so
+      args.key <- str_trim(args.keyval.vec[1], side="both")
+      args.val <- str_trim(args.keyval.vec[2], side="both")
+
+# start debug
+#       cat(sprintf('%s: got\n', this.fn))
+#       cat('\targs.keyval.list==\n')
+#       print(args.keyval.list)
+#       cat('\targs.keyval.vec==\n')
+#       print(args.keyval.vec)
+#       cat(sprintf('\targs.key==%s\n', args.key))
+#       cat(sprintf('\targs.val==%s\n', args.val))
+#   end debug
+
+      # A real case statement would be nice to have
+      if        (args.key == 'netcdf.fp') {
+        netcdf.fp <- args.val
+      } else if (args.key == 'var.name') {
+        var.name <- args.val
+      } else {
+        stop(sprintf("unknown argument='%s'", args.key))
+        # TODO: show usage
+        q(status=1) # exit with error
+      }
+    } # end for loop over arguments
+
+    # payload!
+    netCDF.stats.to.stdout(netcdf.fp, var.name)
+
+  } # end if testing number of arguments
+} # end if (!interactive())
