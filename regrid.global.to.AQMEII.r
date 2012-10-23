23eca0a182e56a20271dda84c364ebc91e111f70
Tom Roche <Tom_Roche@pobox.com>
2012-10-23 14:28:08 -0400

    regrid.global.to.AQMEII.r: mostly fixed raster::plot-ing (thanks, Rob Pinder!)
    RWP suggested
    
    * change CRS to match map, i.e., x,y units=m (not km)
    - out.crs <- '+proj=lcc +lat_1=33 +lat_2=45 +lat_0=40 +lon_0=-97 +x_0=-2556 +y_0=-1728'
    + out.crs <- '+proj=lcc +lat_1=33 +lat_2=45 +lat_0=40 +lon_0=-97 +x_0=-2556000 +y_0=-1728000'
    
    which made me notice a typo in my CRS !-( I had been using
    - out.crs <- '+proj=lcc +lat_1=33 +lat_2=45 +lat_0=40 +lon_0=-97 +x=_0=-2556 +y_0=-1728'
    
    * make template.raster and out.raster use same CRS (out.crs)
    - previously template.raster was created with arg=crs, but out.raster was not
    + changed both to take arg=crs
    no change:  still gives (as before)
    class       : RasterLayer
    dimensions  : 299, 459, 137241  (nrow, ncol, ncell)
    resolution  : 53369.55, 56883.69  (x, y)
    extent      : -14802449, 9694173, -6258782, 10749443  (xmin, xmax, ymin, ymax)
    coord. ref. : +proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0
    data source : /home/rtd/code/R/GEIA_to_netCDF/GEIA_N2O_oceanic_regrid.nc
    names       : N2O.emissions
    zvar        : emi_n2o
    
    me@terra:~/code/R/GEIA_to_netCDF $ ./regrid_GEIA_netCDF.sh
    produces
    https://github.com/downloads/TomRoche/GEIA_to_netCDF/GEIA_N2O_oceanic_regrid.pdf
    in which
    + page=1 (using raster::plot) has the correct orientation! but needs cropping (i.e., correct extents) and better map, legends, titling
    ~ page=2 (using raster::plot) same as p1, i.e., setting plot extents failed
    ~ page=3 (using raster::plot) same as p1, i.e., use of raster::crop failed
    - page=4 (using fields::image.plot): data and map are mirrored: see
      https://stat.ethz.ch/pipermail/r-sig-geo/2012-October/016467.html
    
    TODO:
    * fix raster::plot extents
    * give raster::plot's better map, legends, titling
    * fix fields::image.plot map/data orientation (then tweak)

--- regrid.global.to.AQMEII.r
+++ regrid.global.to.AQMEII.r
@@ -27,13 +27,19 @@ in.band <- Sys.getenv('DATA_INPUT_BAND')
 data.var.name <- Sys.getenv('DATA_VAR_NAME')
 template.in.fp <- Sys.getenv('TEMPLATE_INPUT_FP')
 template.band <- Sys.getenv('TEMPLATE_INPUT_BAND')
-out.crs <- '+proj=lcc +lat_1=33 +lat_2=45 +lat_0=40 +lon_0=-97 +x=_0=-2556 +y_0=-1728'
+# coordinate reference system
+# out.crs <- '+proj=lcc +lat_1=33 +lat_2=45 +lat_0=40 +lon_0=-97 +x_0=-2556 +y_0=-1728'
+# Per RWP, make x,y units=meters (to match map, etc)
+out.crs <- '+proj=lcc +lat_1=33 +lat_2=45 +lat_0=40 +lon_0=-97 +x_0=-2556000 +y_0=-1728000'
 out.fp <- Sys.getenv('DATA_OUTPUT_FP')
+
+# package=raster map, for raster::plot
 map.us.unproj <- wrld_simpl[wrld_simpl$ISO3 == 'USA', ]  # unprojected
 map.us.proj <- spTransform(map.us.unproj, CRS(out.crs))  # projected
-
+# package=M3 map for fields::image.plot
 map.table.fp <- Sys.getenv('MAP_TABLE_FP')
 map.cmaq <- read.table(map.table.fp, sep=",")
+
 palette.vec <- c(
 # original from KMF, 3 colors added to get deciles in probabilities.vec
   #              R color
@@ -110,8 +116,8 @@ in.raster
 # z-value     : 0 
 # zvar        : emi_n2o 
 
-# make template raster
-# truncate 'ncdump' to avoid IOAPI cruft
+# make template raster for extents
+# look @ input to template/extents raster: truncate 'ncdump' to avoid IOAPI cruft
 system(sprintf('ncdump -h %s | head -n 13', template.in.fp))
 # netcdf emis_mole_all_20080101_12US1_cmaq_cb05_soa_2008ab_08c.EXTENTS_INPUT {
 # dimensions:
@@ -126,29 +132,29 @@ system(sprintf('ncdump -h %s | head -n 13', template.in.fp))
 #                 emi_n2o:var_desc = "Model species XYL                                                               " ;
 
 template.in.raster <- raster(template.in.fp, varname=data.var.name, band=template.band)
-template.raster <- projectExtent(template.in.raster, out.crs)
+template.raster <- projectExtent(template.in.raster, crs=out.crs)
 # Warning message:
 # In projectExtent(template.in.raster, out.crs) :
 #   158 projected point(s) not finite
 
-# is that "projected point(s) not finite" warning important? Probably not.
+# is that "projected point(s) not finite" warning important? Probably not, per Hijmans
 
 template.raster
-# class       : RasterLayer
+# should resemble the domain specification @
+# https://github.com/TomRoche/cornbeltN2O/wiki/AQMEII-North-American-domain#wiki-EPA
+# using CRS with x,y units=m
+# class       : RasterLayer 
 # dimensions  : 299, 459, 137241  (nrow, ncol, ncell)
 # resolution  : 53369.55, 56883.69  (x, y)
-# extent      : -12246449, 12250173, -4532510, 12475715  (xmin, xmax, ymin, ymax)
-# coord. ref. : +proj=lcc +lat_1=33 +lat_2=45 +lat_0=40 +lon_0=-97 +y_0=-1728 +ellps=WGS84
-# values      : none
-
-# That looks good! Much more like the domain specification @
-# https://github.com/TomRoche/cornbeltN2O/wiki/AQMEII-North-American-domain#wiki-EPA
+# extent      : -14802449, 9694173, -6258782, 10749443  (xmin, xmax, ymin, ymax)
+# coord. ref. : +proj=lcc +lat_1=33 +lat_2=45 +lat_0=40 +lon_0=-97 +x_0=-2556000 +y_0=-1728000 +ellps=WGS84 
 
 out.raster <-
   projectRaster(
-    from=in.raster, to=template.raster, method='bilinear',
+#    from=in.raster, to=template.raster, method='bilinear', # why no CRS?
+    from=in.raster, to=template.raster, method='bilinear', crs=out.crs,
     overwrite=TRUE, progress='window', format='CDF',
-    # args from ?writeRaster
+    # args from writeRaster
     NAflag=-999.0,  # match emi_n2o:missing_value,_FillValue (TODO: copy)
     varname=data.var.name, 
     varunit='ton N2O-N/yr',
@@ -157,14 +163,16 @@ out.raster <-
     yname='ROW',
     filename=out.fp)
 out.raster
-# class       : RasterLayer 
-# dimensions  : 299, 459, 137241  (nrow, ncol, ncell)
-# resolution  : 53369.55, 56883.69  (x, y)
-# extent      : -12246449, 12250173, -4532510, 12475715  (xmin, xmax, ymin, ymax)
-# coord. ref. : +proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0 
-# data source : /tmp/projectRasterTest/GEIA_N2O_oceanic_regrid.nc 
-# names       : N2O.emissions 
-# zvar        : emi_n2o 
+# made with CRS
+#> class       : RasterLayer 
+#> dimensions  : 299, 459, 137241  (nrow, ncol, ncell)
+#> resolution  : 53369.55, 56883.69  (x, y)
+#> extent      : -14802449, 9694173, -6258782, 10749443  (xmin, xmax, ymin, ymax)
+# ??? why still proj=longlat ???
+#> coord. ref. : +proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0 
+#> data source : /home/rtd/code/R/GEIA_to_netCDF/GEIA_N2O_oceanic_regrid.nc
+#> names       : N2O.emissions 
+#> zvar        : emi_n2o 
 
 system(sprintf('ls -alht %s', test.dir))
 system(sprintf('ncdump -h %s', out.fp))
@@ -174,18 +182,20 @@ system(sprintf('ncdump -h %s', out.fp))
 #   ROW = 299 ;
 # variables:
 #   double COL(COL) ;
-#     COL:units = "meter" ;
+#       COL:units = "meter" ;
+#       COL:long_name = "COL" ;
 #   double ROW(ROW) ;
-#     ROW:units = "meter" ;
+#       ROW:units = "meter" ;
+#       ROW:long_name = "ROW" ;
 #   float emi_n2o(ROW, COL) ;
-#     emi_n2o:units = "ton N2O-N/yr" ;
-#     emi_n2o:missing_value = -999.f ;
-#     emi_n2o:_FillValue = -999.f ;
-#     emi_n2o:long_name = "N2O emissions" ;
-#     emi_n2o:projection = "+proj=lcc +lat_1=33 +lat_2=45 +lat_0=40 +lon_0=-97 +y_0=-1728 +ellps=WGS84" ;
-#     emi_n2o:projection_format = "PROJ.4" ;
-#     emi_n2o:min = 5.9605e-08f ;
-#     emi_n2o:max = 783.92f ;
+#       emi_n2o:units = "ton N2O-N/yr" ;
+#       emi_n2o:missing_value = -999.f ;
+#       emi_n2o:_FillValue = -999.f ;
+#       emi_n2o:long_name = "N2O emissions" ;
+#       emi_n2o:projection = "+proj=lcc +lat_1=33 +lat_2=45 +lat_0=40 +lon_0=-97 +x_0=-2556000 +y_0=-1728000 +ellps=WGS84" ;
+#       emi_n2o:projection_format = "PROJ.4" ;
+#       emi_n2o:min = 5.9605e-08f ;
+#       emi_n2o:max = 783.92f ;
 
 netCDF.stats.to.stdout(netcdf.fp=out.fp, var.name=data.var.name)
 # For /tmp/projectRasterTest/GEIA_N2O_oceanic_regrid.nc var=emi_n2o
@@ -210,7 +220,6 @@ plot(map.us.proj, add=TRUE)
 
 # plot page 2: try setting plot extents-------------------------------
 plot(out.raster, ext=template.raster)
-# add a projected CONUS map
 plot(map.us.proj, add=TRUE)
 
 # plot page 3: try raster::crop---------------------------------------
@@ -227,10 +236,9 @@ netCDF.stats.to.stdout(netcdf.fp=out.fp, var.name=data.var.name)
 #   end debugging
 
 plot(out.raster.crop)
-# add a projected CONUS map
 plot(map.us.proj, add=TRUE)
 
-# plot page 4: try image.plot-----------------------------------------
+# start image.plot----------------------------------------------------
 
 # plot.raster(
 #   raster=out.raster,
@@ -242,34 +250,36 @@ plot(map.us.proj, add=TRUE)
 # )
 # step through plot.raster
 
-title="foo"
-subtitle="bar"
-q.vec=probabilities.vec
-# *.centers may need reversed, as in `y.centers <- raster.centers.x(out.raster)`: see below
+title <- "foo"
+subtitle <- "bar"
+q.vec <- probabilities.vec
 x.centers <- raster.centers.x(out.raster)
 y.centers <- raster.centers.y(out.raster)
 # fails to set dimensions correctly! -> [1] 137241      1
 out.data <- as.matrix(values(out.raster))
-dim(out.data) <- c( length(y.centers), length(x.centers) )
-# start debugging
-dim(out.data)
-#   end debugging
-
-# plot.data(out.data, title, subtitle, x.centers, y.centers, q.vec, colors, map.cmaq
-# step through plot.data
-plot.list <- list(y=x.centers, x=y.centers, z=out.data)
-# note the reversal above! does not throw error in image.plot, but doesn't look right, either
 quantiles <- quantile(c(out.data), q.vec, na.rm=TRUE)
 quantiles.formatted <- format(as.numeric(quantiles), digits=3)
+
+# page 4--------------------------------------------------------------
+# close! data is recognizable, but North America position is mirrored
+
+dim(out.data) <- c(length(x.centers), length(y.centers)) # cols, rows
+# start debugging
+dim(out.data)
+#> [1] 459 299
+#   end debugging
+
+# plot.data(out.data, title, subtitle, x.centers, y.centers, q.vec, colors, map.cmaq)
+# step through plot.data
+plot.list <- list(x=x.centers, y=y.centers, z=out.data)
 
 image.plot(plot.list, xlab="", ylab="", axes=F, col=colors(100),
   axis.args=list(at=quantiles, labels=quantiles.formatted),
   main=title)
-# produces distorted image: diagonal streaks
 # map(add=TRUE) # not defined here
 lines(map.cmaq)
 
-# plot page 4: end image.plot-----------------------------------------
+#   end image.plot----------------------------------------------------
 
 # flush to the plot device
 dev.off()
